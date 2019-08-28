665bd321099402e87934edec0d611fe7a3c006e9
haider <haider.shah@hotmail.com>
2019-08-28 03:27:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,18 +9,20 @@ end
 
 def add_element_to_end_of_array(array, element)
   array.push(element)
+  
 end
 
 def add_element_to_start_of_array(array, element)
+  array.unshift(element)
   
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.unshift(element)
 end
 
 def retrieve_element_from_index(array, index_number)
